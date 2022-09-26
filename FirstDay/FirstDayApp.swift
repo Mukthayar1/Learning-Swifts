f5e7922db1fa72489e1ac38a3c6ed56ac161c972
Linkitsoft <73120628+Linkitsoft@users.noreply.github.com>
2022-09-26 18:06:03 +0500

    Initial Commit

---
+++ FirstDay/FirstDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstDayApp.swift
+//  FirstDay
+//
+//  Created by mac on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
